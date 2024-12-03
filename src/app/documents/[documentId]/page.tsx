8b4b8866f36f77174d055645b844094d24c370ad
Adle25 <askaradilet41@gmail.com>
2024-12-03 19:10:42 +0500

    added editor store

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -6,8 +6,6 @@ interface DocumentIdPageProps {
 }
 
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
-    const { documentId } = await params
-
     return (
         <div className="min-h-screen bg-[#FAFBFD]">
             <Toolbar />
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { cn } from "@/lib/utils";
-import { LucideIcon } from "lucide-react";
+import { useEditorStore } from "@/store/use-editor-store";
+import { LucideIcon, Undo2Icon } from "lucide-react";
 
 interface ToolbarButtonProps {
     onClick?: () => void;
@@ -19,9 +22,28 @@ const ToolbarButton = ({ onClick, isActive, icon: Icon }: ToolbarButtonProps) =>
 };
 
 export const Toolbar = () => {
+    const { editor } = useEditorStore();
+
+    const sections: {
+        label: string;
+        icon: LucideIcon;
+        onClick: () => void;
+        isActive?: boolean;
+    }[][] = [
+            [
+                {
+                    label: "Undo",
+                    icon: Undo2Icon,
+                    onClick: () => editor?.chain().focus().undo().run(),
+                }
+            ]
+        ];
+
     return (
         <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
-            Toolbar
+            {sections[0].map((item) => (
+                <ToolbarButton key={item.label} {...item} />
+            ))}
         </div>
     );
 };
